715c189a324da6669525445343c7c672430e2a92
YOUR_GITHUB_USERNAME <106437135+samsonsim2@users.noreply.github.com>
2023-01-04 00:45:36 +0800

    remove headers for post request

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -137,15 +137,7 @@ export const LandingPage = () => {
             data: {
               image: { src },
             },
-          } = await axios.post(
-            `${process.env.REACT_APP_SERVER_URL}`,
-            formData,
-            {
-              headers: {
-                'Content-Type': 'multipart/form-data',
-              },
-            }
-          )
+          } = await axios.post(`${process.env.REACT_APP_SERVER_URL}`, formData)
 
           // problem is that setImage is asynchronous. So axios.post is invoked before setImage is set, therefore upload a null value
 
